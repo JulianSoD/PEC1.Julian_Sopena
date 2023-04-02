adeb17d1966614efe0f41304d2d146e355fffb64
Julián Sopeña <126665932+JulianSoD@users.noreply.github.com>
2023-04-02 19:55:12 +0200

    Update GestionColegios.java

--- pec1_julian_sopena/src/edu/ucj/programacion/pec1/julian_sopena_dominguez/GestionColegios.java
+++ pec1_julian_sopena/src/edu/ucj/programacion/pec1/julian_sopena_dominguez/GestionColegios.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class GestionColegios {
 	public static void colocarAlumnos (Alumno[] alumno,Colegio colegio){
 		for (int i=0; i<alumno.length;i++) {
-			Colegio.asignarAsiento(alumno[i]);
-			if (i > (colegio.getAulas().length)*3) {
-				System.out.println("No hay suficientes asientos para todos los alumnos.");
-				break;
-			}
+			Colegio.asignarAsiento(alumno[i]); // Entiendo que al pedir: por cada objeto Alumno que recibe 
+							   // el método se debe asignar al primer asiento libre que tenga cualquiera de las aulas
+							   // quiere utilizar el método asignarAlumno que hemos creado
 		}
 	}	
 	public static void main(String[] args) {
